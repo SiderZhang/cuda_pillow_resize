2128a20055f1b220d6840e0dc89b2cea4ccf00f2
siderzhangPC <siderzhang@qq.com>
2024-04-23 00:10:40 +0800

    init push

---
+++ include/libcuda.h
@@ -0,0 +1,11 @@
+//
+// Created by siderzhangPC on 2024/4/22.
+//
+
+#ifndef LIBJPEGTEST_LIBCUDA_H
+#define LIBJPEGTEST_LIBCUDA_H
+
+extern "C" void resize(unsigned int source_xsize, unsigned int source_ysize, unsigned char* image_pixel_data, int channels,
+            unsigned int target_xsize, unsigned int target_ysize, unsigned char* result_pixel_data, double *normalized_pixel_data);
+
+#endif //LIBJPEGTEST_LIBCUDA_H
